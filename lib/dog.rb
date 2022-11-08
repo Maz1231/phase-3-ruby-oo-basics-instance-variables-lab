6344fac467b7706161f31dadb8b1d46455090c6a
Mazin Idris <mazin1231@hotmail.com>
2022-11-07 18:08:22 -0700

    commit tree 3c19daddb056ced181b5ce930c0e1a7b8baac4d9

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,17 @@
 class Dog
 
-end
+    def name =(value)
+        @name = value 
+    end 
+    
+    def name 
+     @name
+    end 
+end 
+
+
+odie = Dog.name 
+
+odie.name = "Odie"
+
+puts odie.name 
